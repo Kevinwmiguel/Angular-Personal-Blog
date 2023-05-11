d24ccad1f1877729462918c253c3d108c1b92a03
Kevin Willians Miguel <kevincursos@outlook.com>
2023-05-11 17:49:13 +0100

    updateBlog

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuBarComponent } from './menu-bar/menu-bar.component';
+import { SmallCardComponent } from './small-card/small-card.component';
+import { BigCardComponent } from './big-card/big-card.component';
+import { MenuTitleComponent } from './menu-title/menu-title.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuBarComponent,
+    SmallCardComponent,
+    BigCardComponent,
+    MenuTitleComponent
   ],
   imports: [
     BrowserModule,
